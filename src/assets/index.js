1fbf0c3cc1714e66f9b0a89b8fdd14c046e3900e
Alpacorp <alejandro.palacios88@gmail.com>
2022-11-28 23:08:13 -0500

    change logos, open graph and images brands

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,39 +1,40 @@
-import one from './steps/1.svg';
-import two from './steps/2.svg';
-import three from './steps/3.svg';
-import four from './steps/4.svg';
-import five from './steps/5.svg';
-import six from './steps/6.svg';
-import onePage from './steps/1P.svg';
-import twoPage from './steps/2P.svg';
-import threePage from './steps/3P.svg';
-import fourPage from './steps/4P.svg';
-import fivePage from './steps/5P.svg';
-import sixPage from './steps/6P.svg';
-import sevenPage from './steps/7P.svg';
-import seven from './steps/7.svg';
-import dayone from './days/16.svg';
-import daytwo from './days/17.svg';
-import daythree from './days/18.svg';
-import dayfour from './days/19.svg';
-import dayfive from './days/20.svg';
-import daysix from './days/21.svg';
-import dayseven from './days/22.svg';
-import dayeight from './days/23.svg';
-import daynine from './days/24.svg';
-import beforeIcon from './components/before.svg';
-import afterIcon from './components/after.svg';
-import decorUp from './components/decoracion_arriba.svg';
-import decorDown from './components/decoracion_abajo.svg';
-import mainFamily from './components/navidad_belen.svg';
-import novenaIntro from './components/novena_aguinaldo.svg';
-import footer from './components/decoracion_abajo.svg';
-import brandLogoIntro from './components/sulogoaquiintro.svg';
-import Servientrega from '../assets/brands/logo-Servientrega.png';
-import Star from '../assets/brands/star.svg';
-import MainTitle from '../assets/components/main-title.svg';
-import onOff from '../assets/brands/on-off.svg';
-import logosTry from '../assets/brands/logos_try.svg';
+import one from "./steps/1.svg";
+import two from "./steps/2.svg";
+import three from "./steps/3.svg";
+import four from "./steps/4.svg";
+import five from "./steps/5.svg";
+import six from "./steps/6.svg";
+import onePage from "./steps/1P.svg";
+import twoPage from "./steps/2P.svg";
+import threePage from "./steps/3P.svg";
+import fourPage from "./steps/4P.svg";
+import fivePage from "./steps/5P.svg";
+import sixPage from "./steps/6P.svg";
+import sevenPage from "./steps/7P.svg";
+import seven from "./steps/7.svg";
+import dayone from "./days/16.svg";
+import daytwo from "./days/17.svg";
+import daythree from "./days/18.svg";
+import dayfour from "./days/19.svg";
+import dayfive from "./days/20.svg";
+import daysix from "./days/21.svg";
+import dayseven from "./days/22.svg";
+import dayeight from "./days/23.svg";
+import daynine from "./days/24.svg";
+import beforeIcon from "./components/before.svg";
+import afterIcon from "./components/after.svg";
+import decorUp from "./components/decoracion_arriba.svg";
+import decorDown from "./components/decoracion_abajo.svg";
+import mainFamily from "./components/navidad_belen.svg";
+import novenaIntro from "./components/novena_aguinaldo.svg";
+import footer from "./components/decoracion_abajo.svg";
+import brandLogoIntro from "./components/sulogoaquiintro.svg";
+import Servientrega from "../assets/brands/logo-Servientrega.png";
+import Star from "../assets/brands/star.svg";
+import MainTitle from "../assets/components/main-title.svg";
+import onOff from "../assets/brands/on-off.svg";
+import logosTry from "../assets/brands/logos_try.svg";
+import logosTry2 from "../assets/brands/logos_try2_b.svg";
 
 export {
   one,
@@ -71,5 +72,6 @@ export {
   sixPage,
   sevenPage,
   onOff,
-  logosTry
-}
+  logosTry,
+  logosTry2,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,58 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import './Pages.css';
-import { one, two, three, four, five, six, seven, MainTitle, onOff, logosTry } from '../assets';
-import BrandLogo from '../Components/BrandLogo';
-import Header from '../Components/Header';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import "./Pages.css";
+import {
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  MainTitle,
+  onOff,
+  logosTry2,
+} from "../assets";
+import BrandLogo from "../Components/BrandLogo";
+import Header from "../Components/Header";
 
 const Home = () => {
   return (
     <>
       <Header show={false} />
-      <div className='brandZone'>
+      <div className="brandZone">
         <BrandLogo src={onOff} />
         <BrandLogo src={MainTitle} />
-        <div className='logosTry'>
-          <img src={logosTry} alt="logos try" />
+        <div className="logosTry">
+          <img src={logosTry2} alt="logos try" />
         </div>
       </div>
-      {/* <p className='instructive'>Sigue cada paso  y reza en familia</p> */}
-      <div className='mainMenu'>
-        <Link to='/stepone' className='menuOption'>
+      <p className="instructive">Sigue cada paso y reza en familia</p>
+      <div className="mainMenu">
+        <Link to="/stepone" className="menuOption">
           <img src={one} alt="step one" />
         </Link>
-        <Link to='/steptwo' className='menuOption'>
+        <Link to="/steptwo" className="menuOption">
           <img src={two} alt="step two" />
         </Link>
-        <Link to='/stepthree' className='menuOption'>
+        <Link to="/stepthree" className="menuOption">
           <img src={three} alt="step three" />
         </Link>
-        <Link to='/stepfour' className='menuOption'>
+        <Link to="/stepfour" className="menuOption">
           <img src={four} alt="step four" />
         </Link>
-        <Link to='/stepfive' className='menuOption'>
+        <Link to="/stepfive" className="menuOption">
           <img src={five} alt="step five" />
         </Link>
-        <Link to='/stepSix' className='menuOption'>
+        <Link to="/stepSix" className="menuOption">
           <img src={six} alt="step six" />
         </Link>
-        <Link to='/stepseven' className='menuOption'>
+        <Link to="/stepseven" className="menuOption">
           <img src={seven} alt="step seven" />
         </Link>
       </div>
     </>
-  )
+  );
 };
 
 export default Home;
